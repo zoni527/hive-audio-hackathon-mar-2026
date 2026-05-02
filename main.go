f6f69527584824a70b52f46f6817c282df7ef9c2
Johnny Varila <jvarila@student.hive.fi>
2026-05-02 13:50:50 +0300

    fix: recovered accidentally deleted final state of the project that wasn't part of the git history... PHEW!!!

--- main.go
+++ main.go
@@ -12,16 +12,25 @@ import (
 	"github.com/gordonklaus/portaudio"
 )
 
-const PI float64 = math.Pi
-const BUFFER_SIZE int32 = 1024
+const PI = math.Pi
+const BUFFER_SIZE = 1024
 const WINDOW_HEIGHT int32 = 1000
 const WINDOW_WIDTH int32 = 1800
-const TARGET_FPS int32 = 500
-const AMPLITUDE_GRAPH_AMPLITUDE int32 = 500
-const FFT_GRAPH_AMPLITUDE int32 = 300
-const FFT_GRAPH_AMPLITUDE_Y_OFFSET int32 = 500
+const TARGET_FPS = 500
+const AMPLITUDE_GRAPH_AMPLITUDE = WINDOW_HEIGHT/2 - 200
+const FFT_GRAPH_AMPLITUDE = 100
+const FFT_GRAPH_AMPLITUDE_Y_OFFSET = WINDOW_HEIGHT
+const SAMPLE_GRAPH_AMPLITUDE float64 = 500
+const POLAR_BASE_RADIUS float64 = 200.0
+const POLAR_AMPLITUDE float64 = 180.0
+const POLAR_THICKNESS = 3
+const GRAVITY = 0.95
 
-type vis struct {
+type pixel struct {
+	x, y int32
+}
+
+type rect struct {
 	x, y, w, h int32
 }
 
@@ -37,13 +46,14 @@ func fft(a []complex128, invert bool) {
 		a0[i] = a[2*i]
 		a1[i] = a[2*i+1]
 	}
-	go fft(a0, invert)
-	go fft(a1, invert)
 
+	fft(a0, invert)
+	fft(a1, invert)
 	ang := 2 * PI / float64(n)
 	if invert {
 		ang *= -1
 	}
+
 	w := complex(1, 0)
 	wn := complex(math.Cos(ang), math.Sin(ang))
 	for i := 0; 2*i < n; i++ {
@@ -78,7 +88,9 @@ func main() {
 		largestPowerOfTwo *= 2
 	}
 	largestPowerOfTwo /= 2
+
 	complexNumbers := make([]complex128, largestPowerOfTwo)
+	visualHeights := make([]float64, largestPowerOfTwo/2)
 
 	stream, err := portaudio.OpenDefaultStream(1, 0, 44100, len(buf), buf)
 	chk(err)
@@ -88,26 +100,36 @@ func main() {
 	var time, sampleMemorySize uint64
 	sampleMemorySize = 20
 	var max int32
-	var fmax, fprev, textSize, boxSize float64
-	points := make([]vis, WINDOW_WIDTH)
+	var fmax, fprev, boxSize float64
+	rects := make([]rect, WINDOW_WIDTH)
 	sampleMemory := make([]float64, sampleMemorySize)
+	polarPoints := make([]pixel, len(buf))
 	for !rl.WindowShouldClose() {
 		chk(stream.Read())
 
-		// Look for maximum element & form complex numbers for fft
+		// Loop over samples in buffer
 		max = 0
 		for i, v := range buf {
+			num := float64(v) / math.MaxInt32
 			if i < largestPowerOfTwo {
-				num := float64(v) / float64(math.MaxInt32)
-				complexNumbers[i] = complex(num, 0)
+				windowingFactor := (0.5 - 0.5*math.Cos(2*PI*float64(i)/float64(len(buf))))
+				complexNumbers[i] = complex(num*windowingFactor, 0)
 			}
 			if v > max {
 				max = v
 			}
+
+			// Calculate points for polar coordinate visualizer
+			centerX := WINDOW_WIDTH / 2
+			centerY := WINDOW_HEIGHT / 2
+			angle := float64(i) * 2 * PI / float64(len(buf))
+			sampleX := centerX + int32(math.Cos(angle)*(POLAR_BASE_RADIUS+num*POLAR_AMPLITUDE))
+			sampleY := centerY + int32(math.Sin(angle)*(POLAR_BASE_RADIUS+num*POLAR_AMPLITUDE))
+			polarPoints[i] = pixel{sampleX, sampleY}
 		}
-		fmax = float64(max) / float64(math.MaxInt32)
-		// Threshold
-		if fmax > 0.1 {
+
+		fmax = float64(max) / math.MaxInt32
+		if fmax > 0.1 { // Threshold
 			sampleMemory[time%sampleMemorySize] = fmax
 		} else {
 			sampleMemory[time%sampleMemorySize] = 0
@@ -124,31 +146,81 @@ func main() {
 
 		// Scaling drawable elements
 		boxSize = 0.1*fprev + 0.9*fmax
-		textSize = averageSample
-		// fmt.Printf("max:\t%v\n", fmax)
-		// fmt.Println(complexNumbers)
-
+		rects[time%uint64(WINDOW_WIDTH)] = rect{w: 2, h: int32(float64(AMPLITUDE_GRAPH_AMPLITUDE) * boxSize)}
+		bgCol := rl.Color{R: 30, G: 30, B: 40, A: 255}
 		rl.BeginDrawing()
+		rl.ClearBackground(bgCol)
 
-		rl.ClearBackground(rl.RayWhite)
-		speedy := 0.01
-		phasey := float64(time) * speedy
-		points[time%uint64(WINDOW_WIDTH)] = vis{w: 2, h: int32(float64(AMPLITUDE_GRAPH_AMPLITUDE) * boxSize)}
-		for i := range points {
-			rl.DrawRectangle(points[i].x, points[i].y, points[i].w, points[i].h, rl.Red)
-			points[i].x += 1
+		// Sample visualizer
+		for i, v := range buf {
+			num := float64(v) / float64(math.MaxInt32)
+			height := int32(num * SAMPLE_GRAPH_AMPLITUDE)
+			leftMargin := int32((WINDOW_WIDTH - BUFFER_SIZE) / 2)
+			rl.DrawRectangle(leftMargin+int32(i), WINDOW_HEIGHT/2-height/2, 1, height, rl.Purple)
 		}
+
+		// Loudness visualizer
+		loudnessColor := rl.Red
+		loudnessColor.A = 180
+		for i := range rects {
+			rl.DrawRectangle(rects[i].x, rects[i].y, rects[i].w, rects[i].h, loudnessColor)
+			rects[i].x += 1
+		}
+
+		// FFT visualizer
+		fftCol := rl.Green
+		fftCol.A = 180
 		for i := range len(complexNumbers) / 2 {
-			offset := float64(i) * (float64(WINDOW_WIDTH) / float64(len(complexNumbers)/2))
+			offset := int32(2 * float64(i) * (float64(WINDOW_WIDTH) / float64(len(complexNumbers)/2)))
 			width := WINDOW_WIDTH/int32(len(complexNumbers)) + 3
-			height := int32(cmplx.Abs(complexNumbers[i]) * float64(FFT_GRAPH_AMPLITUDE))
-			rl.DrawRectangle(int32(offset), FFT_GRAPH_AMPLITUDE_Y_OFFSET, width, height, rl.Green)
+			magnitude := cmplx.Abs(complexNumbers[i])
+			height := math.Log1p(magnitude) * float64(FFT_GRAPH_AMPLITUDE)
+			if height > visualHeights[i] {
+				visualHeights[i] = height
+			} else {
+				visualHeights[i] *= GRAVITY
+			}
+			rl.DrawRectangle(offset,
+				FFT_GRAPH_AMPLITUDE_Y_OFFSET-int32(visualHeights[i]),
+				width, int32(visualHeights[i]),
+				fftCol)
 		}
-		rl.DrawText("Congrats! You created your first window!", 100, 500+int32(math.Sin(phasey)*100.0), 20+int32(textSize*300), rl.Black)
 
+		// Centered text
+		textString := "ENJOY THE SOUNDS!"
+		textSize := float32(80)
+		textSpacing := float32(2)
+		textFont := rl.GetFontDefault()
+		textDimensions := rl.MeasureTextEx(textFont, textString, textSize, textSpacing)
+		textPosition := rl.Vector2{
+			X: float32(WINDOW_WIDTH / 2),
+			Y: float32(WINDOW_HEIGHT / 2),
+		}
+		textRotation := float32(0)
+		textOrigin := rl.Vector2{
+			X: textDimensions.X / 2,
+			Y: textDimensions.Y / 2,
+		}
+		textCol := bgCol
+		rl.DrawTextPro(
+			textFont,
+			textString,
+			textPosition,
+			textOrigin,
+			textRotation,
+			textSize, textSpacing,
+			textCol,
+		)
+
+		// Polar coordinates sample visualizer
+		polarCol := rl.Color{R: 200, G: 200, B: 200, A: 255}
+		for _, v := range polarPoints {
+			rl.DrawCircle(v.x, v.y, POLAR_THICKNESS, polarCol)
+		}
 		rl.EndDrawing()
 		fprev = boxSize
 
+		// Closing signal
 		select {
 		case <-sig:
 			return
